604450e4bfdc4dac3ee795d3d3ba16893fc46d47
jesusmunozr <jesus.munozr@gmail.com>
2021-02-15 19:00:19 -0500

    Restaurant class created

--- DeliveryDrone/Direction.cs
+++ DeliveryDrone/Direction.cs
@@ -1,10 +1,10 @@
 namespace DeliveryDrone
 {
-    public enum Direction : string
+    public class Direction
     {
-        North = "N",
-        South = "S",
-        East = "E",
-        West = "W"
+        public const string North = "N";
+        public const string South = "S";
+        public const string East = "E";
+        public const string West = "W";
     }
 }
--- DeliveryDrone/Position.cs
+++ DeliveryDrone/Position.cs
@@ -6,6 +6,6 @@ namespace DeliveryDrone
     {
         public int PosX { get; set; }
         public int PosY { get; set; }
-        public Direction Direction { get; set; }
+        public string Direction { get; set; }
     }
 }
